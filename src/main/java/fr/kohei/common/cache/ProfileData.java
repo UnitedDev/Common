3b4c100f64e0a026acbdcf0fdd1e8732375bfd23
Hugo <58655174+Rhodless@users.noreply.github.com>
2022-06-13 01:53:47 +0200

    [+] Changed Profile informations (1.5.0)

--- src/main/java/fr/kohei/common/cache/ProfileData.java
+++ src/main/java/fr/kohei/common/cache/ProfileData.java
@@ -12,37 +12,43 @@ import java.util.List;
 @Getter
 @Setter
 public class ProfileData implements Serializable {
-	private String displayName;
-	private String rankName;
-	private int coins, hosts;
-	private String language;
-	private List<String> silentPlayer;
-	private List<String> friends;
-	private boolean friendRequests;
-	private boolean privateMessages;
-	private boolean notifications;
-	private final HashMap<String, Integer> checkViolations;
-	private Division division;
-	private int experience;
+    private String displayName;
+    private String rankName;
+    private int coins, hosts;
+    private String language;
+    private List<String> silentPlayer;
+    private List<String> friends;
+    private boolean friendRequests;
+    private boolean privateMessages;
+    private boolean notifications;
+    private final HashMap<String, Integer> checkViolations;
+    private Division division;
+    private int experience;
+    private long playTime;
+    private final List<String> ips;
+    private String link;
 
-	private final HashMap<String, Object> serversData;
+    private final HashMap<String, Object> serversData;
 
-	public ProfileData(Rank rank, int coins, int hosts, String language) {
-		this.rankName = rank.token();
-		this.coins = coins;
-		this.hosts = hosts;
-		this.language = language;
-		this.silentPlayer = new ArrayList<>();
-		this.friends = new ArrayList<>();
-		this.friendRequests = true;
-		this.privateMessages = true;
-		this.notifications = true;
-		this.division = Division.AUCUNE;
-		this.serversData = new HashMap<>();
-		this.checkViolations = new HashMap<>();
-	}
+    public ProfileData(Rank rank, int coins, int hosts, String language) {
+        this.rankName = rank.token();
+        this.coins = coins;
+        this.hosts = hosts;
+        this.language = language;
+        this.silentPlayer = new ArrayList<>();
+        this.friends = new ArrayList<>();
+        this.friendRequests = true;
+        this.privateMessages = true;
+        this.notifications = true;
+        this.division = Division.AUCUNE;
+        this.serversData = new HashMap<>();
+        this.checkViolations = new HashMap<>();
+        this.playTime = 1;
+        this.ips = new ArrayList<>();
+        this.link = null;
+    }
 
-	public Rank getRank() {
-		return RedisProvider.redisProvider.getRanks().stream().filter(rank -> rank.token().equalsIgnoreCase(rankName)).findFirst().orElse(RedisProvider.redisProvider.getRank("default").get());
-	}
+    public Rank getRank() {
+        return RedisProvider.redisProvider.getRanks().stream().filter(rank -> rank.token().equalsIgnoreCase(rankName)).findFirst().orElse(RedisProvider.redisProvider.getRank("default").get());
+    }
 }
--- src/main/java/fr/kohei/common/cache/Report.java
+++ src/main/java/fr/kohei/common/cache/Report.java
@@ -1,18 +1,23 @@
 package fr.kohei.common.cache;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
+import java.util.Date;
 import java.util.UUID;
 
+@Setter
 @Getter
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class Report {
 
+    private final Date date;
     private final UUID reportId;
     private final UUID uuid;
     private final UUID reporter;
     private final String message;
-    private final boolean resolved;
+    private boolean resolved;
 
 }
